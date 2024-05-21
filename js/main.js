23fea364c99f6b1c4e2ef1a3720ec6cf1325ee42
dhymas1st <66336773+dhymas1st@users.noreply.github.com>
2024-05-21 19:59:05 -0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('#cep').mask('00000-000',{
+      placeholder: '00000-000'
+    });
+    $('#cpf').mask('000.000.000-00', {
+      reverse: true,
+      placeholder: '000.000.000-00'
+    });
+    $('#telefone').mask('(00) 00000-0000',{
+      placeholder: '(00) 00000-0000'
+    });
+  });
